7e8f54405f666eb9264fa2965f0c288762b81b8d
leandro.v.059@gmail.com <leandro.v.059@gmail.com@f342e48f-5086-dbd9-1d7d-ae1893eebf0d>
2014-07-11 20:08:36 +0000

    Subo la funcionalidad para mostrar/ocultar las bicicleterias y estaciones de bicicletas en el mapa. Saqué el botón para mostrar talleres porque me parece que son lo mismo que las bicicleterias y tampoco encontré información en buenosaires.data sobre talleres como algo separado de las bicicleterias.

--- CityBike/src/com/citybike/FragmentMapa.java
+++ CityBike/src/com/citybike/FragmentMapa.java
@@ -1,5 +1,7 @@
 package com.citybike;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,46 +14,64 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 
 public class FragmentMapa extends Fragment {
 	private GoogleMap mMap;	
-	private static final LatLng HAMBURG = new LatLng(-34.7, -58.32);
+	private final LatLng CENTRO = new LatLng(-34.60, -58.38);
+	private final String CSVBicicleterias = "bicicleterias.csv";
+	private final String CSVEstaciones = "estaciones.csv";
+	private ArrayList<Marker> bicicleterias = new ArrayList<Marker>();
+	private ArrayList<Marker> estaciones = new ArrayList<Marker>();
+	private boolean mostrandoBicicleterias = false;
+	private boolean mostrandoEstaciones = false;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {		 
 		View view = inflater.inflate(R.layout.fragment_pantalla_principal, null, false);		
 		setUpMapIfNeeded();		
 		
-		
 		Button b_bicicleteria = (Button) view.findViewById(R.id.id_b_bicleteria);
-		Button b_talleres = (Button) view.findViewById(R.id.id_b_talleres);
+		//Button b_talleres = (Button) view.findViewById(R.id.id_b_talleres);
 		Button b_estaciones = (Button) view.findViewById(R.id.id_b_bicisendas);
 		Button b_rutas = (Button) view.findViewById(R.id.id_b_rutas);
 		
 		b_bicicleteria.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
-		        Toast.makeText(getActivity(), "Mostrar bicicleterias en el mapa", Toast.LENGTH_SHORT).show();				
+		        //Toast.makeText(getActivity(), "Mostrar bicicleterias en el mapa", Toast.LENGTH_SHORT).show();
+				if (!mostrandoBicicleterias){
+					mostrandoBicicleterias = true;					
+				}else{
+					mostrandoBicicleterias = false;
+				}
+				setVisibilidadBicicleterias(mostrandoBicicleterias); 
 			}
 		});
 		
-		b_talleres.setOnClickListener(new OnClickListener() {			
+		/*b_talleres.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
 		        Toast.makeText(getActivity(), "Mostrar talleres en el mapa", Toast.LENGTH_SHORT).show();
 				
 			}
-		});
+		});*/
 		
 		b_estaciones.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
-		        Toast.makeText(getActivity(), "Mostrar bicisendas en el mapa", Toast.LENGTH_SHORT).show();
-				
+		        //Toast.makeText(getActivity(), "Mostrar bicisendas en el mapa", Toast.LENGTH_SHORT).show();
+		        if (!mostrandoEstaciones){
+		        	mostrandoEstaciones = true;					
+				}else{
+					mostrandoEstaciones = false;
+				}
+				setVisibilidadEstaciones(mostrandoEstaciones); 
 			}
 		});
 		
@@ -84,8 +104,11 @@ public class FragmentMapa extends Fragment {
     }
 	
 	private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(HAMBURG).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CENTRO, 11));
+        colocarMarcadoresBicicleterias(CSVBicicleterias);
+        colocarMarcadoresEstaciones(CSVEstaciones);
+        setVisibilidadBicicleterias(mostrandoBicicleterias);
+        setVisibilidadEstaciones(mostrandoEstaciones);
     }
 
 
@@ -107,7 +130,80 @@ public class FragmentMapa extends Fragment {
 	    }		
 		super.onDestroyView();
 	}
-
-
+	
+	private ArrayList<MarkerOptions> generarMarcadoresBicicleterias(String rutaCSV){
+		ParserCSV.cargarParser(getResources());
+		ArrayList<String[]> lineas = ParserCSV.parsearArchivo(rutaCSV, 9);
+		ArrayList<MarkerOptions> marcadores = new ArrayList<MarkerOptions>();
+		LatLng posicion = null;
+		String titulo = "";
+		String detalle = "";
+		boolean encabezado = true;
+		for(String[] linea : lineas){
+			if(!encabezado){
+				if(linea.length == 9){
+					posicion = new LatLng( Float.valueOf(linea[6]), Float.valueOf(linea[7]) );
+					titulo = linea[0];
+					detalle = linea[1] + " / " + linea[2] + " / " + linea[3];
+					marcadores.add( new MarkerOptions().position( posicion ).title( titulo ).snippet( detalle )
+							.visible(false).icon(BitmapDescriptorFactory.fromResource(R.drawable.icono_bicicleteria)));
+				}
+			}else{
+				encabezado = false;
+			}
+		}
+		 
+		return marcadores;		
+	}
+		
+	private void colocarMarcadoresBicicleterias(String rutaCSV){
+		ArrayList<MarkerOptions> moBicicleterias = generarMarcadoresBicicleterias(rutaCSV);
+		
+		for(MarkerOptions marcador : moBicicleterias){
+			bicicleterias.add( mMap.addMarker(marcador) );
+		}
+	}
+	
+	private void setVisibilidadBicicleterias(boolean mostrar){
+		for(Marker marcador : bicicleterias){
+			marcador.setVisible(mostrar);
+		}
+	}
+	
+	private ArrayList<MarkerOptions> generarMarcadoresEstaciones(String rutaCSV){
+		ParserCSV.cargarParser(getResources());
+		ArrayList<String[]> lineas = ParserCSV.parsearArchivo(rutaCSV, 5);
+		ArrayList<MarkerOptions> marcadores = new ArrayList<MarkerOptions>();
+		LatLng posicion = null;
+		String titulo = "";
+		boolean encabezado = true;
+		for(String[] linea : lineas){
+			if(!encabezado){
+				if(linea.length == 5){
+					posicion = new LatLng( Float.valueOf(linea[3]), Float.valueOf(linea[4]) );
+					titulo = linea[1];
+					marcadores.add( new MarkerOptions().position( posicion ).title( titulo ).visible(false)
+							.icon(BitmapDescriptorFactory.fromResource(R.drawable.estaciones24x32)));
+				}
+			}else{
+				encabezado = false;
+			}
+		}
+		 
+		return marcadores;		
+	}
+	
+	private void colocarMarcadoresEstaciones(String rutaCSV){
+		ArrayList<MarkerOptions> moEstaciones = generarMarcadoresEstaciones(rutaCSV);
+		for(MarkerOptions marcador : moEstaciones){
+			estaciones.add( mMap.addMarker(marcador) );
+		}
+	}
+	
+	private void setVisibilidadEstaciones(boolean mostrar){
+		for(Marker marcador : estaciones){
+			marcador.setVisible(mostrar);
+		}
+	}
 	
 }
---
+++ CityBike/src/com/citybike/ParserCSV.java
@@ -0,0 +1,45 @@
+package com.citybike;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import android.content.res.Resources;
+
+public class ParserCSV {
+	
+	private static Resources recursos;
+	
+	public static void cargarParser(Resources recursos){
+		ParserCSV.recursos = recursos;
+	}
+	
+	public static ArrayList<String[]> parsearArchivo(String nombreArchivo, int cantCampos){
+		BufferedReader br = null;
+		ArrayList<String[]> lineas = new ArrayList<String[]>();
+		
+		try{
+			//File csv = new File(rutaArchivo);
+			//FileInputStream fis = new FileInputStream( csv );
+			//BufferedInputStream bis = new BufferedInputStream( fis );
+			InputStreamReader isr = new InputStreamReader( recursos.getAssets().open(nombreArchivo) );
+			br = new BufferedReader( isr );
+			
+			String linea = null;
+			String[] campos;
+			while( (linea = br.readLine()) != null ){
+				campos = linea.split(";");
+				lineas.add(campos);
+			}
+			br.close();
+		}catch(Exception e){
+			System.out.println("Error al parsear archivo");
+		}
+		
+		return lineas;
+	}
+}
