ee1fdd65262b477a787b5a07ad767aba3510d508
leandro.v.059@gmail.com <leandro.v.059@gmail.com@f342e48f-5086-dbd9-1d7d-ae1893eebf0d>
2014-07-22 00:43:41 +0000

    Agrego funcionalidad para eliminar contactos (como los cambios no se persisten, sólo funciona la primera vez)

--- CityBike/src/com/citybike/pantallainicio/Fragments/FragmentContactos.java
+++ CityBike/src/com/citybike/pantallainicio/Fragments/FragmentContactos.java
@@ -12,20 +12,51 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 public class FragmentContactos extends ListFragment{
+	
+	ArrayAdapter<ContactsRowManager> adapter;
+	private Button b_eliminar;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 	        
-        View rootView = inflater.inflate(R.layout.layout_fragment_contactos, container, false);
+        View view = inflater.inflate(R.layout.layout_fragment_contactos, container, false);
                 
-        ArrayAdapter<ContactsRowManager> adapter = new ContactListArrayAdapter(this, getModel());
+        adapter = new ContactListArrayAdapter(this, getModel());
 
         setListAdapter(adapter);
-        return rootView;
+        
+        if (view !=null){
+			b_eliminar = (Button) view.findViewById(R.id.id_b_eliminar_seleccionados);
+        }
+        
+        return view;
     }
-	  
+	
+	@Override
+	public void onViewCreated(View view,Bundle savedInstanceState) {
+		super.onViewCreated(view,savedInstanceState);
+		b_eliminar.setOnClickListener(new OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				List<ContactsRowManager> contacts = getModel();				
+				int removedCount = 0;
+				for(int i = 0; i < adapter.getCount(); i++){
+					if( adapter.getItem(i).isSelected() ){
+						contacts.remove(i - removedCount);
+						removedCount++;
+					}
+				}
+				adapter.clear();
+				adapter.addAll(contacts);
+			}
+		});
+	}
+	
 	//Contactos hardcodeados - después habrá que tomarlos de algún archivo que haga de BD
 	private List<ContactsRowManager> getModel() {
 		List<ContactsRowManager> list = new ArrayList<ContactsRowManager>();
@@ -41,7 +72,7 @@ public class FragmentContactos extends ListFragment{
 	private ContactsRowManager get(String s) {
 		return new ContactsRowManager(s);
 	}
-
+	
 	   
 	/*@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
