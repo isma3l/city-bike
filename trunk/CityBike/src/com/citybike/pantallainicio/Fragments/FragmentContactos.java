d89ebb086debbdde4c7da4393972240e4489871e
leandro.v.059@gmail.com <leandro.v.059@gmail.com@f342e48f-5086-dbd9-1d7d-ae1893eebf0d>
2014-07-25 00:58:46 +0000

    Agrego persistencia a la lista de contactos. Se pueden agregar y eliminar, y los cambios quedan guardados.
    Creé 2 archivos de tipo SharedPreferences (http://developer.android.com/guide/topics/data/data-storage.html#pref): uno hace de base de datos de usuarios (la llené con nuestros datos, o sea que hay 7 usuarios "registrados"). El otro es donde se guardan los contactos del usuario. Así que si alguna otra pantalla de la aplicación necesita acceder a la lista de contactos (invitar contactos a un circuito, por ejemplo), debería obtenerla de este último archivo.
    
    Para agregar contactos, elijan la opción en el ActionMenu de la pantalla de contactos (el menu de arriba a la derecha); les va a salir una ventana de dialogo donde tienen que poner el mail del usuario a agregar (los mails los puse como "nombre.apellido@gmail.com" para no complicarme). Por ejemplo, para agregarme a mi pongo leandro.miguenz@gmail.com. Si ponen un mail que no está en la base-de-datos-trucha, se muestra un toast que dice que el usuario no existe.
    
    Falta verificación de contactos duplicados.

--- trunk/CityBike/src/com/citybike/pantallainicio/Fragments/FragmentContactos.java
+++ trunk/CityBike/src/com/citybike/pantallainicio/Fragments/FragmentContactos.java
@@ -1,25 +1,151 @@
 package com.citybike.pantallainicio.Fragments;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import com.citybike.MainActivity;
 import com.citybike.R;
 import com.citybike.pantallacontactos.ContactListArrayAdapter;
-import com.citybike.pantallacontactos.ContactsRowManager;
+import com.citybike.pantallacontactos.ContactModel;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FragmentContactos extends ListFragment{
 	
-	ArrayAdapter<ContactsRowManager> adapter;
-	private Button b_eliminar;
+	ArrayAdapter<ContactModel> adapter;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+    	inflater.inflate(R.menu.menu_opciones_contactos, menu);
+	}
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.agregar:
+    		showAddContactPopUp();
+    		break;
+    	case R.id.eliminar:
+    		removeSelectedContacts();
+    	default:
+    		break;
+    	}
+
+    	return true;
+    } 
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+	        
+        View view = inflater.inflate(R.layout.layout_fragment_contactos, container, false);
+        adapter = new ContactListArrayAdapter(this, getModel());
+        setListAdapter(adapter);       
+        return view;
+    }
+	
+	private void removeSelectedContacts(){
+		ArrayList<ContactModel> forRemove = new ArrayList<ContactModel>();
+		for(int i = 0; i < adapter.getCount(); i++){
+			if( adapter.getItem(i).isSelected() ){
+				forRemove.add( adapter.getItem(i) );
+			}
+		}
+		applyRemovals(forRemove);
+	}
+	
+	//Contactos hardcodeados - despu�s habr� que tomarlos de alg�n archivo que haga de BD
+	private List<ContactModel> getModel() {
+		List<ContactModel> list = new ArrayList<ContactModel>();
+		
+		//Restauro el estado de la lista de contactos
+	    SharedPreferences contacts = getActivity().getSharedPreferences(MainActivity.CONTACTSDATABASE, 0);
+	    @SuppressWarnings("unchecked")
+	    HashMap<String, String> contactsHash = (HashMap<String, String>) contacts.getAll();
+	    
+	    for( String key : contactsHash.keySet() ){
+	    	list.add( get(contactsHash.get(key), key) );
+	    }
+	    
+		/*list.add(get("Contacto1"));
+		list.add(get("Contacto2"));
+		list.add(get("Contacto3"));
+		list.add(get("Contacto4"));
+		list.add(get("Contacto5"));*/
+		
+		return list;
+	}
+
+	private void showAddContactPopUp() {
+		AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this.getActivity());
+		helpBuilder.setTitle("Agregar contacto");
+		helpBuilder.setMessage("Ingrese el mail del usuario que desea agregar");
+		final EditText input = new EditText(this.getActivity());
+		input.setSingleLine();
+		input.setText("");
+		helpBuilder.setView(input);
+		helpBuilder.setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int which) {
+		    	addContact( input.getText().toString() );
+		    }
+		});
+
+		// Remember, create doesn't show the dialog
+		AlertDialog helpDialog = helpBuilder.create();
+		helpDialog.show();
+	}
+	
+	//Obtengo las shared preferences USERSDATABASE y consulto si existe el usuario
+	private void addContact(String userMail){
+		SharedPreferences registeredUsers = getActivity().getSharedPreferences(MainActivity.USERSDATABASE, 0);
+		if( !registeredUsers.contains(userMail) ){
+			Toast.makeText(getActivity(), "El usuario no existe", Toast.LENGTH_SHORT).show();
+		}else{
+			SharedPreferences contacts = getActivity().getSharedPreferences(MainActivity.CONTACTSDATABASE, 0);
+			SharedPreferences.Editor editor = contacts.edit();
+			String userName = registeredUsers.getString(userMail, "");
+		    editor.putString(userMail, userName );
+		    //Comiteo los cambios al archivo
+		    editor.commit();
+		    
+			adapter.add( get( userName, userMail ) );		
+		}
+	}
+	
+	private void applyRemovals(List<ContactModel> contacts){
+		//Elimino los contactos del archivo de contactos
+		SharedPreferences contactsDB = getActivity().getSharedPreferences(MainActivity.CONTACTSDATABASE, 0);
+		SharedPreferences.Editor editor = contactsDB.edit();
+		
+		for(ContactModel contact : contacts ){
+			editor.remove( contact.getMail() );
+			adapter.remove(contact); //esta linea elimina los contactos del adapter
+		}
+		editor.commit();
+	}
+	
+	private ContactModel get(String name, String mail) {
+		return new ContactModel(name, mail);
+	}
 	
 	public static FragmentContactos newInstance(Bundle args){
 		FragmentContactos fragmentContactos=new FragmentContactos();
@@ -27,58 +153,6 @@ public class FragmentContactos extends ListFragment{
 			fragmentContactos.setArguments(args);
 		return fragmentContactos;
 	}
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-	        
-        View view = inflater.inflate(R.layout.layout_fragment_contactos, container, false);
-                
-        adapter = new ContactListArrayAdapter(this, getModel());
-
-        setListAdapter(adapter);
-        
-        if (view !=null){
-			b_eliminar = (Button) view.findViewById(R.id.id_b_eliminar_seleccionados);
-        }
-        
-        return view;
-    }
-	
-	@Override
-	public void onViewCreated(View view,Bundle savedInstanceState) {
-		super.onViewCreated(view,savedInstanceState);
-		b_eliminar.setOnClickListener(new OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				List<ContactsRowManager> contacts = getModel();				
-				int removedCount = 0;
-				for(int i = 0; i < adapter.getCount(); i++){
-					if( adapter.getItem(i).isSelected() ){
-						contacts.remove(i - removedCount);
-						removedCount++;
-					}
-				}
-				adapter.clear();
-				adapter.addAll(contacts);
-			}
-		});
-	}
-	
-	//Contactos hardcodeados - despu�s habr� que tomarlos de alg�n archivo que haga de BD
-	private List<ContactsRowManager> getModel() {
-		List<ContactsRowManager> list = new ArrayList<ContactsRowManager>();
-		list.add(get("Contacto1"));
-		list.add(get("Contacto2"));
-		list.add(get("Contacto3"));
-		list.add(get("Contacto4"));
-		list.add(get("Contacto5"));
-		//list.get(1).setSelected(true);
-		return list;
-	}
-
-	private ContactsRowManager get(String s) {
-		return new ContactsRowManager(s);
-	}
-	
 	   
 	/*@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
