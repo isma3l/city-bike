6435eb25cccfa14e57046ff515b1564a55ee3564
aliluis@gmail.com <aliluis@gmail.com@f342e48f-5086-dbd9-1d7d-ae1893eebf0d>
2014-07-30 09:47:31 +0000

    Se loguea con usuario de facebook, pero falta ordenar visualmente la imagen y el nombre de  usuario traido como dato desde facebook

--- trunk/CityBike/src/com/citybike/pantallainicio/Fragments/SelectionFragment.java
+++ trunk/CityBike/src/com/citybike/pantallainicio/Fragments/SelectionFragment.java
@@ -1,17 +1,47 @@
 package com.citybike.pantallainicio.Fragments;
 
-import java.util.Arrays;
-
-import com.citybike.R;
-import com.facebook.widget.LoginButton;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.citybike.R;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.ProfilePictureView;
 
 public class SelectionFragment extends Fragment {
+	private ProfilePictureView profilePictureView;
+	private TextView userNameView;
+	private UiLifecycleHelper uiHelper;
+	private static final int REAUTH_ACTIVITY_CODE = 100;
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REAUTH_ACTIVITY_CODE) {
+	        uiHelper.onActivityResult(requestCode, resultCode, data);
+	    }
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+	    uiHelper.onCreate(savedInstanceState);
+	}
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+	    @Override
+	    public void call(final Session session, final SessionState state, final Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
 	public static SelectionFragment newInstance(Bundle args){
 		SelectionFragment selectionFragment=new SelectionFragment();
 		if (args !=null) 
@@ -22,10 +52,75 @@ public class SelectionFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.layout_pantalla_inicio, 
+		View view = inflater.inflate(R.layout.fragment_pantalla_principal, 
 	            					container,
 	            					false);
+		profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
+		profilePictureView.setCropped(true);
+		userNameView = (TextView) view.findViewById(R.id.selection_user_name);
+		checkForAnOpenSession();
 	    return view;
 	}
+	private void checkForAnOpenSession() {
+	    Session session = Session.getActiveSession();
+	    if (session != null && session.isOpened()) {
+	        // Get the user's data
+	        makeMeRequest(session);
+	    }
+		
+	}
 
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    uiHelper.onResume();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle bundle) {
+	    super.onSaveInstanceState(bundle);
+	    uiHelper.onSaveInstanceState(bundle);
+	}
+
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+	private void makeMeRequest(final Session session) {
+	    // Make an API call to get user data and define a 
+	    // new callback to handle the response.
+	    Request request = Request.newMeRequest(session, 
+	            new Request.GraphUserCallback() {
+	        @Override
+	        public void onCompleted(GraphUser user, Response response) {
+	            // If the response is successful
+	            if (session == Session.getActiveSession()) {
+	                if (user != null) {
+	                    // Set the id for the ProfilePictureView
+	                    // view that in turn displays the profile picture.
+	                    profilePictureView.setProfileId(user.getId());
+	                    // Set the Textview's text to the user's name.
+	                    userNameView.setText(user.getName());
+	                }
+	            }
+	            if (response.getError() != null) {
+	                // Handle errors, will do so later.
+	            }
+	        }
+	    });
+	    request.executeAsync();
+	} 
+	private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
+	    if (session != null && session.isOpened()) {
+	        // Get the user's data.
+	        makeMeRequest(session);
+	    }
+	}
 }
